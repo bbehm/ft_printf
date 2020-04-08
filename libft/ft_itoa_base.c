d1284e6e80ec943158a36e214ad427dbedc7c349
bbehm <bbehm@student.fi>
2020-04-08 17:55:42 +0300

    libft added

---
+++ libft/ft_itoa_base.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbehm <bbehm@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/23 17:47:44 by bbehm             #+#    #+#             */
+/*   Updated: 2020/02/23 17:52:43 by bbehm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+#include <stdlib.h>
+
+/*
+** 'A' or 'a' should be passed to c to indicate uper or lower case
+*/
+
+static char	ft_calculate_char(int mod, char c)
+{
+	char	return_char;
+
+	if (mod > 36)
+		return (0);
+	return_char = '0';
+	while (mod--)
+	{
+		return_char++;
+		if (return_char == ':')
+			return_char = c;
+	}
+	return (return_char);
+}
+
+static int	ft_get_len(uintmax_t num, uintmax_t base)
+{
+	int		len;
+
+	len = 0;
+	while (num)
+	{
+		num /= base;
+		len++;
+	}
+	return (len);
+}
+
+static char	*ft_generate_string(uintmax_t num, uintmax_t base, char c)
+{
+	uintmax_t	sum;
+	int			mod;
+	int			len;
+	int			i;
+	char		*str;
+
+	if (num == 0)
+	{
+		if (!(str = ft_strnew(1)))
+			return (NULL);
+		*str = '0';
+		return (str);
+	}
+	sum = num;
+	i = 0;
+	len = ft_get_len(num, base);
+	if (!(str = ft_strnew(len)))
+		return (NULL);
+	while (sum)
+	{
+		mod = sum % base;
+		sum /= base;
+		str[(len--) - 1] = ft_calculate_char(mod, c);
+	}
+	return (str);
+}
+
+char		*ft_itoa_base_mod(uintmax_t num, uintmax_t base, char c)
+{
+	char	*str;
+
+	str = ft_generate_string(num, base, c);
+	return (str);
+}
---
+++ libft/ft_strndup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbehm <bbehm@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/23 16:40:13 by bbehm             #+#    #+#             */
+/*   Updated: 2020/02/23 16:41:53 by bbehm            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+char	*ft_strndup(const char *s, size_t n)
+{
+	char			*res;
+	unsigned int	i;
+
+	i = 0;
+	res = (char*)malloc(sizeof(char) * (n + 1));
+	if (res == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		res[i] = s[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
