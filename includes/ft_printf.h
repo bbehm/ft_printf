93aea58366c6d04d7911c6d62f5eb9ac9faabd78
Bianca Behm <bbehm@c1r5p5.hive.fi>
2020-07-02 17:59:35 +0300

    norme error fixed

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bbehm <bbehm@student.hive.fi>              +#+  +:+       +#+        */
+/*   By: bbehm <bbehm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 13:02:03 by bbehm             #+#    #+#             */
-/*   Updated: 2020/06/25 17:10:31 by bbehm            ###   ########.fr       */
+/*   Updated: 2020/07/02 17:49:56 by bbehm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int				ft_printf(const char *format, ...);
 void			parse(t_tab *tab, const char *format);
 void			initialize(t_tab *tab);
 void			fix_precision(t_tab *tab, const char *format);
-void 			fix_width(t_tab *tab, const char *format);
+void			fix_width(t_tab *tab, const char *format);
 void			do_the_d(t_tab *tab);
 void			do_the_c(t_tab *tab);
 void			do_the_s(t_tab *tab);
@@ -59,8 +59,8 @@ void			do_the_o(t_tab *tab, char flag);
 void			do_the_u(t_tab *tab);
 void			do_the_x(t_tab *tab, char flag);
 void			do_the_p(t_tab *tab, char flag);
-void    		percent_flag(t_tab *tab);
-void    		do_the_f(t_tab *tab);
+void			percent_flag(t_tab *tab);
+void			do_the_f(t_tab *tab);
 void			do_gap(t_tab *tab, char c, int len, int new_len);
 void			show_char(wint_t c, t_tab *tab);
 void			fix_h(t_tab *tab, const char *str);
